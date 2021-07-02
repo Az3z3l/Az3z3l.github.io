501f4adf883948adc2e4b85a6a903ad01908687c
Az3z3l <yaswant.yme@gmail.com>
2021-07-02 17:10:18 +0530

    remove projects

---
+++ static/script/header.js
@@ -0,0 +1,7 @@
+document.getElementById("headerz").innerHTML = ` 
+    <h1 class="az3z3l">Az3z3l</h1>
+        <a href="/">Posts</a>&emsp;&emsp;
+        <a href="/about/">About</a>
+    <br />
+    <br />
+`
