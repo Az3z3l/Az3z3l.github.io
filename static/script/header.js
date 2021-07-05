689529752621a07f8d4be4ed68664d14d2ff1b51
Az3z3l <yaswant.yme@gmail.com>
2021-07-05 17:06:53 +0530

    ga??

--- static/script/header.js
+++ static/script/header.js
@@ -5,3 +5,15 @@ document.getElementById("headerz").innerHTML = `
     <br />
     <br />
 `
+
+document.querySelector("head").innerHTML += `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-20F3YZJW4C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-20F3YZJW4C');
+</script>
+`
